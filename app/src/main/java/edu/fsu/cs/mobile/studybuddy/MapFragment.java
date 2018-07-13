ee18ea192ab0f67f8c8d1b6331c2c72140d0f316
sfinn13 <sarita.finn@gmail.com>
2018-07-13 19:07:49 -0400

    Added simple map feature

--- app/src/main/java/edu/fsu/cs/mobile/studybuddy/MapFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/studybuddy/MapFragment.java
@@ -6,12 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
 
 
     public MapFragment() {
@@ -23,7 +31,19 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        mapFrag.getMapAsync(this);
+
+        return rootView;
     }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng libLocation = new LatLng(30.4450, -84.2999);
+        map.addMarker(new MarkerOptions().position(libLocation).title("Marker in Dirac Library"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(libLocation, 18));
+    }
 }
