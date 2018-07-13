5b3af997653e2f90428279bb3aac8576916d68a4
Romario Estrella <rde16@my.fsu.edu>
2018-07-13 02:59:58 -0400

    fixed authentication

--- app/src/main/java/edu/fsu/cs/mobile/studybuddy/SignUpActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/studybuddy/SignUpActivity.java
@@ -1,5 +1,6 @@
 package edu.fsu.cs.mobile.studybuddy;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,8 +30,8 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText email, password, name;
     private Button register;
 
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference mRef;
+    //private FirebaseDatabase mDatabase;
+    //private DatabaseReference mRef;
 
     //private FirebaseStorage mStorageRef;
     @Override
@@ -38,7 +39,7 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        mDatabase = FirebaseDatabase.getInstance();
+       /// mDatabase = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -62,13 +63,15 @@ public class SignUpActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             String userID = mAuth.getCurrentUser().getUid();
-                            mRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+                            //mRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
 
                             String regName = name.getText().toString();
                             Map newUser = new HashMap();
                             newUser.put("Name", regName);
 
-                            mRef.setValue(newUser);
+                            //mRef.setValue(newUser);
+                            startActivity(new Intent(SignUpActivity.this, StudyBuddy.class));
+
 
                         }
                         else{
