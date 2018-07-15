85b9c0ac3a8c70015e42eb3535ac2bffdb03238b
sfinn13 <sarita.finn@gmail.com>
2018-07-15 14:57:34 -0400

    Implemented an onMarkerClick funct

--- app/src/main/java/edu/fsu/cs/mobile/studybuddy/MapFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/studybuddy/MapFragment.java
@@ -6,26 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment implements OnMapReadyCallback{
-
+public class MapFragment extends Fragment implements
+        GoogleMap.OnMarkerClickListener,
+        OnMapReadyCallback{
 
     public MapFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,5 +45,15 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         LatLng libLocation = new LatLng(30.4450, -84.2999);
         map.addMarker(new MarkerOptions().position(libLocation).title("Marker in Dirac Library"));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(libLocation, 18));
+
+        map.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        String str = "# of users found";
+        marker.setTitle(str);
+
+        return false;
     }
 }
