555b4a57d53dde6606728f9ef688f4120b8124ff
SupamanRD <SupamanRD@users.noreply.github.com>
2018-07-15 15:29:12 -0400

    Login activity added to the manifest. Firebase login authentication implemented in Login activity.

--- app/src/main/java/edu/fsu/cs/mobile/studybuddy/Login.java
+++ app/src/main/java/edu/fsu/cs/mobile/studybuddy/Login.java
@@ -1,17 +1,32 @@
 package edu.fsu.cs.mobile.studybuddy;
 
 import android.app.Activity;
-        import android.os.Bundle;
-        import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
 
         import android.widget.Button;
         import android.widget.EditText;
         import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class Login extends Activity  {
     Button b1;
     EditText ed1,ed2;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +37,59 @@ public class Login extends Activity  {
         ed1 = findViewById(R.id.logEmail);
         ed2 = findViewById(R.id.logPass);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+            }
+        };
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ed1.getText().toString().equals("temp") &&
+                /*if(ed1.getText().toString().equals("temp") &&
                         ed2.getText().toString().equals("temp")) {
                     Toast.makeText(getApplicationContext(),
                             "Redirecting...",Toast.LENGTH_SHORT).show();
                 }else {
                     Toast.makeText(getApplicationContext(),"Wrong Credentials",Toast.LENGTH_SHORT).show();
-                }
+                }*/
+                String email = ed1.getText().toString();
+                String password = ed2.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(Login.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Intent myIntent = new Intent(Login.this, StudyBuddy.class);
+                            startActivity(myIntent);
+                            finish();
+                        }
+                    }
+                });
             }
         });
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
