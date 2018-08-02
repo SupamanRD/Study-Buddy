99837a6dec7a8724909327ef60cbefee8b59953d
SupamanRD <SupamanRD@users.noreply.github.com>
2018-08-01 20:35:01 -0400

    Google Login components implemented

--- app/src/main/java/edu/fsu/cs/mobile/studybuddy/Login.java
+++ app/src/main/java/edu/fsu/cs/mobile/studybuddy/Login.java
@@ -2,8 +2,11 @@ package edu.fsu.cs.mobile.studybuddy;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
         import android.widget.Button;
@@ -11,22 +14,43 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.signin.SignIn;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class Login extends Activity  {
-    Button b1;
-    EditText ed1,ed2;
+public class Login extends AppCompatActivity  {
+    private static final String TAG = "LOGIN_ACTIVITY";
+    private Button b1;
+    private EditText ed1,ed2;
+    private SignInButton googleBtn;
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     private FirebaseDatabase mDatabase;
+    private FirebaseFirestore db;
     private DatabaseReference mRef;
 
     @Override
@@ -37,6 +61,7 @@ public class Login extends Activity  {
         b1 = findViewById(R.id.logButton);
         ed1 = findViewById(R.id.logEmail);
         ed2 = findViewById(R.id.logPass);
+        googleBtn = (SignInButton) findViewById(R.id.googleButton);
 
         TextView textView2 = findViewById(R.id.redirect);
         textView2.setOnClickListener(new View.OnClickListener() {
@@ -51,8 +76,15 @@ public class Login extends Activity  {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
+                /*if(user != null){
+                    Intent myIntent = new Intent(Login.this, StudyBuddy.class);
+                    startActivity(myIntent);
+                    finish();
+                }*/
             }
         };
+
+        db = FirebaseFirestore.getInstance();
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +115,94 @@ public class Login extends Activity  {
             }
         });
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                            Toast.makeText(Login.this, "Connection Failed",
+                                    Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                        else{
+                            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(Login.this);
+                            String personName = acct.getDisplayName();
+                            String personGivenName = acct.getGivenName();
+
+                            String userID = mAuth.getCurrentUser().getUid();
+                            mRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+
+                            //String regName = name.getText().toString();
+                            Map newUser = new HashMap();
+                            newUser.put("Name", personGivenName);
+                            newUser.put("active", "false");
+
+                            mRef.setValue(newUser);
+
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("active", "false");
+                            user.put("name", personGivenName);
+                            db.collection("users")
+                                    .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .set(user);
+                            Intent myIntent = new Intent(Login.this, StudyBuddy.class);
+                            startActivity(myIntent);
+                            finish();
+                        }
+
+
+                        // ...
+                    }
+                });
+    }
+
 
     @Override
     public void onStart() {
